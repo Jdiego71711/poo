731b891a62020dc217d904120e18f57213fac97f
Jdiego71711 <juan.ramirezurib@campusucc.edu.co>
2025-10-01 22:01:24 +0000

    clase9

---
+++ clase7/Persona.java
@@ -0,0 +1,53 @@
+public class Persona {
+
+    //Atributos
+    private int identificacion;
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private double estatura;
+    private double peso;
+
+    //Método constructor de la clase se reconoce porque tiene el mismo nombre de la clase
+    //El constructor de la clase me permite inicializar la clase
+    //la palabra reservada "this" se utiliza para autoreferenciar el atributo de la clase
+    public Persona(int identificacion, String nombres, String apellidos, int edad, double estatura, double peso){
+       this.identificacion = identificacion;
+       this.nombres = nombres;
+       this.apellidos = apellidos;
+       this.edad = edad;
+       this.estatura = estatura;
+       this.peso = peso;
+    }
+
+    //Métodos
+    public void caminar(){
+        System.out.println("Caminar");
+    }
+
+    public boolean jugar(){
+        return true;
+    }
+
+    public void comer(){
+        System.out.println("Comiendo");
+    }
+
+    public String estudiar(String carrera){
+        return "Usted está estudiando: " + carrera;
+    }
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0 && edad < 100) {
+            this.edad = edad;
+        } else {
+            System.out.println("edad invalida");
+        }
+    }
+
+    //Tarea consultar sobre el método toString.
+    
+}
